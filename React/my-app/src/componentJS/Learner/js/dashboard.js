e3d7136278982e15e319b9a7c515fbef53f72ca9
duonglam306 <lamduong11201@gmail.com>
2021-11-02 13:34:48 +0700

    Update LearnerUI

--- React/my-app/src/componentJS/Learner/js/dashboard.js
+++ React/my-app/src/componentJS/Learner/js/dashboard.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "../css/LnDashboard.css"
 import About from "../js/about";
+import CourseForYou from "../js/courseForYou";
 function LnDashboard() {
     return (
         <div id="lnDashboard">
             <About />
+            <CourseForYou />
         </div>
     )
 }
