f5e4604acfba3af7bb892393ef0fe9b37edcf0e3
duonglam306 <lamduong11201@gmail.com>
2021-11-02 17:10:28 +0700

    Update LnCalendar

--- React/my-app/src/componentJS/DbCalendar.js
+++ React/my-app/src/componentJS/DbCalendar.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import Calendar from 'react-calendar'
-import 'react-calendar/dist/Calendar.css'
+import '../componentCSS/dbCalendar.css'
 
 function DbCalendar() {
     const [date, setDate] = useState(new Date());
