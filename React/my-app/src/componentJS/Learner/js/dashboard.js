1228fe61c43f3fff8999d664c3f829439484ce24
duonglam306 <lamduong11201@gmail.com>
2021-11-02 19:04:42 +0700

    Update Calender

--- React/my-app/src/componentJS/Learner/js/dashboard.js
+++ React/my-app/src/componentJS/Learner/js/dashboard.js
@@ -2,11 +2,17 @@ import React from "react";
 import "../css/LnDashboard.css"
 import About from "../js/about";
 import CourseForYou from "../js/courseForYou";
+import Calender             from "../../DbCalendar"
 function LnDashboard() {
     return (
         <div id="lnDashboard">
-            <About />
-            <CourseForYou />
+            <div id="about_CFY">
+                <About />
+                <CourseForYou />
+            </div>
+            <div id="calender">
+                <Calender />
+            </div>
         </div>
     )
 }
--- React/my-app/src/componentJS/Learner/js/learnerUI.js
+++ React/my-app/src/componentJS/Learner/js/learnerUI.js
@@ -8,7 +8,7 @@ import LnReadTutorial       from "./readTutorial";
 import LnReadNotes          from "./readNotes";
 import LnAnnouncements      from "./announcements";
 import LnManageProfile      from "./manageProfile";
-import Calender             from "../../DbCalendar"
+
 import avt from "../../../img/cheems.png";
 
 
@@ -29,7 +29,6 @@ return (
             <Route exact path='/learner/announcements'  component={LnAnnouncements}></Route>
             <Route exact path='/learner/manageprofile'  component={LnManageProfile}></Route>
         </Switch>
-        <div><Calender /></div>
     </Router>
     );
 }
