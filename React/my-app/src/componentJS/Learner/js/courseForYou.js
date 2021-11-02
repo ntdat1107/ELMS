b616b5e43a749daf7a9f16986be87a49ffe77d7a
duonglam306 <lamduong11201@gmail.com>
2021-11-02 15:05:37 +0700

    Update LearnerDB

--- React/my-app/src/componentJS/Learner/js/courseForYou.js
+++ React/my-app/src/componentJS/Learner/js/courseForYou.js
@@ -1,25 +1,118 @@
 import React from 'react'
 import { Scrollbars } from 'react-custom-scrollbars';
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import git from "../img/git.png";
+import ai from "../img/ai.png";
+import django from "../img/django.png";
+import js from "../img/js.png";
+import linux from "../img/linux.png";
+import network from "../img/network.png";
+import star from "../img/star.png";
 function CourseForYou() {
     return (
-        <div id="courses">
-            <p id="title">Courses For You</p>
-            <div id="course">abc
-            </div>
-            <div id="course">ac
-            </div>
-            <div id="course">a
-            </div>
-            <div id="course">a
-            </div>
-            <div id="course">a
-            </div>
-            <div id="course">a
-            </div>
-            <div id="course">a
-            </div>
-            <div id="course">a
+        <div id="coursesCFY">
+            <p id="titleCFY">Courses For You</p>
+            <div id="listCoursesCFY">
+                <Scrollbars>
+                    <div id="courseCFY">
+                        <img id = "imgCourse" src={git} alt = "gitCourse" width = "240" height = "120"></img>
+                        <div id="courseContent">
+                            <p id = "Name">Complete Linux Training Course to Get Your Dream IT Job 2021</p>
+                            <p id = "Desc">The BEST Linux Administration course that prepares you for corporate jobs and for RHCSA, RHCE, LFCS, CLNP certifications.</p>
+                            <p id = "Author">Mr.Long</p>
+                            <div id="rateContent">
+                                <div id="p1"><p>Best Choice</p></div>
+                                <div id="p2"><p><p>4.5</p></p></div>
+                                <div id="p3"><p><img src= {star} alt="star"></img></p></div>
+                                <div id="p4"><p>(10,3005)</p></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="courseCFY">
+                        <img id = "imgCourse" src={git} alt = "gitCourse" width = "240" height = "120"></img>
+                        <div id="courseContent">
+                            <p id = "Name">Complete Linux Training Course to Get Your Dream IT Job 2021</p>
+                            <p id = "Desc">The BEST Linux Administration course that prepares you for corporate jobs and for RHCSA, RHCE, LFCS, CLNP certifications.</p>
+                            <p id = "Author">Mr.Long</p>
+                            <div id="rateContent">
+                                <div id="p1"><p>Best Choice</p></div>
+                                <div id="p2"><p><p>4.5</p></p></div>
+                                <div id="p3"><p><img src= {star} alt="star"></img></p></div>
+                                <div id="p4"><p>(10,3005)</p></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="courseCFY">
+                        <img id = "imgCourse" src={git} alt = "gitCourse" width = "240" height = "120"></img>
+                        <div id="courseContent">
+                            <p id = "Name">Complete Linux Training Course to Get Your Dream IT Job 2021</p>
+                            <p id = "Desc">The BEST Linux Administration course that prepares you for corporate jobs and for RHCSA, RHCE, LFCS, CLNP certifications.</p>
+                            <p id = "Author">Mr.Long</p>
+                            <div id="rateContent">
+                                <div id="p1"><p>Best Choice</p></div>
+                                <div id="p2"><p><p>4.5</p></p></div>
+                                <div id="p3"><p><img src= {star} alt="star"></img></p></div>
+                                <div id="p4"><p>(10,3005)</p></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="courseCFY">
+                        <img id = "imgCourse" src={git} alt = "gitCourse" width = "240" height = "120"></img>
+                        <div id="courseContent">
+                            <p id = "Name">Complete Linux Training Course to Get Your Dream IT Job 2021</p>
+                            <p id = "Desc">The BEST Linux Administration course that prepares you for corporate jobs and for RHCSA, RHCE, LFCS, CLNP certifications.</p>
+                            <p id = "Author">Mr.Long</p>
+                            <div id="rateContent">
+                                <div id="p1"><p>Best Choice</p></div>
+                                <div id="p2"><p><p>4.5</p></p></div>
+                                <div id="p3"><p><img src= {star} alt="star"></img></p></div>
+                                <div id="p4"><p>(10,3005)</p></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="courseCFY">
+                        <img id = "imgCourse" src={git} alt = "gitCourse" width = "240" height = "120"></img>
+                        <div id="courseContent">
+                            <p id = "Name">Complete Linux Training Course to Get Your Dream IT Job 2021</p>
+                            <p id = "Desc">The BEST Linux Administration course that prepares you for corporate jobs and for RHCSA, RHCE, LFCS, CLNP certifications.</p>
+                            <p id = "Author">Mr.Long</p>
+                            <div id="rateContent">
+                                <div id="p1"><p>Best Choice</p></div>
+                                <div id="p2"><p><p>4.5</p></p></div>
+                                <div id="p3"><p><img src= {star} alt="star"></img></p></div>
+                                <div id="p4"><p>(10,3005)</p></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="courseCFY">
+                        <img id = "imgCourse" src={git} alt = "gitCourse" width = "240" height = "120"></img>
+                        <div id="courseContent">
+                            <p id = "Name">Complete Linux Training Course to Get Your Dream IT Job 2021</p>
+                            <p id = "Desc">The BEST Linux Administration course that prepares you for corporate jobs and for RHCSA, RHCE, LFCS, CLNP certifications.</p>
+                            <p id = "Author">Mr.Long</p>
+                            <div id="rateContent">
+                                <div id="p1"><p>Best Choice</p></div>
+                                <div id="p2"><p><p>4.5</p></p></div>
+                                <div id="p3"><p><img src= {star} alt="star"></img></p></div>
+                                <div id="p4"><p>(10,3005)</p></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="courseCFY">
+                        <img id = "imgCourse" src={git} alt = "gitCourse" width = "240" height = "120"></img>
+                        <div id="courseContent">
+                            <p id = "Name">Complete Linux Training Course to Get Your Dream IT Job 2021</p>
+                            <p id = "Desc">The BEST Linux Administration course that prepares you for corporate jobs and for RHCSA, RHCE, LFCS, CLNP certifications.</p>
+                            <p id = "Author">Mr.Long</p>
+                            <div id="rateContent">
+                                <div id="p1"><p>Best Choice</p></div>
+                                <div id="p2"><p><p>4.5</p></p></div>
+                                <div id="p3"><p><img src= {star} alt="star"></img></p></div>
+                                <div id="p4"><p>(10,3005)</p></div>
+                            </div>
+                        </div>
+                    </div>
+                </Scrollbars>
             </div>
         </div>
     )
