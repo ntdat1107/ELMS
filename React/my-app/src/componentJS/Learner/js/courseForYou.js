c700437d96d8d12145356aa0b4ca35213b8809fd
duonglam306 <lamduong11201@gmail.com>
2021-11-02 15:56:33 +0700

    Update CFY

--- React/my-app/src/componentJS/Learner/js/courseForYou.js
+++ React/my-app/src/componentJS/Learner/js/courseForYou.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Scrollbars } from 'react-custom-scrollbars';
 import { Link } from "react-router-dom";
+import CourseForYouCpn from "./courseForYouCpn";
 import git from "../img/git.png";
 import ai from "../img/ai.png";
 import django from "../img/django.png";
@@ -8,110 +9,79 @@ import js from "../img/js.png";
 import linux from "../img/linux.png";
 import network from "../img/network.png";
 import star from "../img/star.png";
+
 function CourseForYou() {
     return (
         <div id="coursesCFY">
             <p id="titleCFY">Courses For You</p>
             <div id="listCoursesCFY">
                 <Scrollbars>
-                    <div id="courseCFY">
-                        <img id = "imgCourse" src={git} alt = "gitCourse" width = "240" height = "120"></img>
-                        <div id="courseContent">
-                            <p id = "Name">Complete Linux Training Course to Get Your Dream IT Job 2021</p>
-                            <p id = "Desc">The BEST Linux Administration course that prepares you for corporate jobs and for RHCSA, RHCE, LFCS, CLNP certifications.</p>
-                            <p id = "Author">Mr.Long</p>
-                            <div id="rateContent">
-                                <div id="p1"><p>Best Choice</p></div>
-                                <div id="p2"><p><p>4.5</p></p></div>
-                                <div id="p3"><p><img src= {star} alt="star"></img></p></div>
-                                <div id="p4"><p>(10,3005)</p></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div id="courseCFY">
-                        <img id = "imgCourse" src={git} alt = "gitCourse" width = "240" height = "120"></img>
-                        <div id="courseContent">
-                            <p id = "Name">Complete Linux Training Course to Get Your Dream IT Job 2021</p>
-                            <p id = "Desc">The BEST Linux Administration course that prepares you for corporate jobs and for RHCSA, RHCE, LFCS, CLNP certifications.</p>
-                            <p id = "Author">Mr.Long</p>
-                            <div id="rateContent">
-                                <div id="p1"><p>Best Choice</p></div>
-                                <div id="p2"><p><p>4.5</p></p></div>
-                                <div id="p3"><p><img src= {star} alt="star"></img></p></div>
-                                <div id="p4"><p>(10,3005)</p></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div id="courseCFY">
-                        <img id = "imgCourse" src={git} alt = "gitCourse" width = "240" height = "120"></img>
-                        <div id="courseContent">
-                            <p id = "Name">Complete Linux Training Course to Get Your Dream IT Job 2021</p>
-                            <p id = "Desc">The BEST Linux Administration course that prepares you for corporate jobs and for RHCSA, RHCE, LFCS, CLNP certifications.</p>
-                            <p id = "Author">Mr.Long</p>
-                            <div id="rateContent">
-                                <div id="p1"><p>Best Choice</p></div>
-                                <div id="p2"><p><p>4.5</p></p></div>
-                                <div id="p3"><p><img src= {star} alt="star"></img></p></div>
-                                <div id="p4"><p>(10,3005)</p></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div id="courseCFY">
-                        <img id = "imgCourse" src={git} alt = "gitCourse" width = "240" height = "120"></img>
-                        <div id="courseContent">
-                            <p id = "Name">Complete Linux Training Course to Get Your Dream IT Job 2021</p>
-                            <p id = "Desc">The BEST Linux Administration course that prepares you for corporate jobs and for RHCSA, RHCE, LFCS, CLNP certifications.</p>
-                            <p id = "Author">Mr.Long</p>
-                            <div id="rateContent">
-                                <div id="p1"><p>Best Choice</p></div>
-                                <div id="p2"><p><p>4.5</p></p></div>
-                                <div id="p3"><p><img src= {star} alt="star"></img></p></div>
-                                <div id="p4"><p>(10,3005)</p></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div id="courseCFY">
-                        <img id = "imgCourse" src={git} alt = "gitCourse" width = "240" height = "120"></img>
-                        <div id="courseContent">
-                            <p id = "Name">Complete Linux Training Course to Get Your Dream IT Job 2021</p>
-                            <p id = "Desc">The BEST Linux Administration course that prepares you for corporate jobs and for RHCSA, RHCE, LFCS, CLNP certifications.</p>
-                            <p id = "Author">Mr.Long</p>
-                            <div id="rateContent">
-                                <div id="p1"><p>Best Choice</p></div>
-                                <div id="p2"><p><p>4.5</p></p></div>
-                                <div id="p3"><p><img src= {star} alt="star"></img></p></div>
-                                <div id="p4"><p>(10,3005)</p></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div id="courseCFY">
-                        <img id = "imgCourse" src={git} alt = "gitCourse" width = "240" height = "120"></img>
-                        <div id="courseContent">
-                            <p id = "Name">Complete Linux Training Course to Get Your Dream IT Job 2021</p>
-                            <p id = "Desc">The BEST Linux Administration course that prepares you for corporate jobs and for RHCSA, RHCE, LFCS, CLNP certifications.</p>
-                            <p id = "Author">Mr.Long</p>
-                            <div id="rateContent">
-                                <div id="p1"><p>Best Choice</p></div>
-                                <div id="p2"><p><p>4.5</p></p></div>
-                                <div id="p3"><p><img src= {star} alt="star"></img></p></div>
-                                <div id="p4"><p>(10,3005)</p></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div id="courseCFY">
-                        <img id = "imgCourse" src={git} alt = "gitCourse" width = "240" height = "120"></img>
-                        <div id="courseContent">
-                            <p id = "Name">Complete Linux Training Course to Get Your Dream IT Job 2021</p>
-                            <p id = "Desc">The BEST Linux Administration course that prepares you for corporate jobs and for RHCSA, RHCE, LFCS, CLNP certifications.</p>
-                            <p id = "Author">Mr.Long</p>
-                            <div id="rateContent">
-                                <div id="p1"><p>Best Choice</p></div>
-                                <div id="p2"><p><p>4.5</p></p></div>
-                                <div id="p3"><p><img src= {star} alt="star"></img></p></div>
-                                <div id="p4"><p>(10,3005)</p></div>
-                            </div>
-                        </div>
-                    </div>
+                    <CourseForYouCpn 
+                        imgSrcCourse = {git} 
+                        altCourse = "GitCourseImage" 
+                        Name = "Complete Linux Training Course to Get Your Dream IT Job 2021" 
+                        Desc="The BEST Linux Administration course that prepares you for corporate jobs and for RHCSA, RHCE, LFCS, CLNP certifications" 
+                        Author="Mr. Long" 
+                        Type="Best Course" 
+                        rateScore="4.5" 
+                        imgStar={star} 
+                        totalRate="(10,123)" 
+                    />
+                    <CourseForYouCpn 
+                        imgSrcCourse = {network} 
+                        altCourse = "NetWorkImage" 
+                        Name = "Learning Network Technology and Security" 
+                        Desc="Learn how to secure and protect your network effectively. An essential course for all system administrators" 
+                        Author="Mr. Dat" 
+                        Type="New" 
+                        rateScore="4.5" 
+                        imgStar={star} 
+                        totalRate="(9999)" 
+                    />
+                    <CourseForYouCpn 
+                        imgSrcCourse = {linux} 
+                        altCourse = "LinuxImage" 
+                        Name = "Complete Linux Training Course to Get Your Dream IT Job 2021" 
+                        Desc="The BEST Linux Administration course that prepares you for corporate jobs and for RHCSA, RHCE, LFCS, CLNP certifications" 
+                        Author="Mr. Duy" 
+                        Type="New" 
+                        rateScore="4.5" 
+                        imgStar={star} 
+                        totalRate="(11113)" 
+                    />
+                    <CourseForYouCpn 
+                        imgSrcCourse = {ai} 
+                        altCourse = "AIImage" 
+                        Name = "Complete Linux Training Course to Get Your Dream IT Job 2021" 
+                        Desc="The BEST Linux Administration course that prepares you for corporate jobs and for RHCSA, RHCE, LFCS, CLNP certifications" 
+                        Author="Mr. Long" 
+                        Type="Best Course" 
+                        rateScore="4.5" 
+                        imgStar={star} 
+                        totalRate="(10,000)" 
+                    />
+                    <CourseForYouCpn 
+                        imgSrcCourse = {js} 
+                        altCourse = "JSImage" 
+                        Name = "Complete Linux Training Course to Get Your Dream IT Job 2021" 
+                        Desc="The BEST Linux Administration course that prepares you for corporate jobs and for RHCSA, RHCE, LFCS, CLNP certifications" 
+                        Author="Mr. Long" 
+                        Type="New" 
+                        rateScore="4.5" 
+                        imgStar={star} 
+                        totalRate="(12345)" 
+                    />
+                    <CourseForYouCpn 
+                        imgSrcCourse = {django} 
+                        altCourse = "DjangoImage" 
+                        Name = "Complete Linux Training Course to Get Your Dream IT Job 2021" 
+                        Desc="The BEST Linux Administration course that prepares you for corporate jobs and for RHCSA, RHCE, LFCS, CLNP certifications" 
+                        Author="Mr. Long" 
+                        Type="Best Course" 
+                        rateScore="4.5" 
+                        imgStar={star} 
+                        totalRate="(9876)" 
+                    />
                 </Scrollbars>
             </div>
         </div>
