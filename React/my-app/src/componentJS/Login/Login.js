73e583bd3f039a11acfe49b4cf5ba80182a65052
Jinnux <nguyentandat26032001@gmail.com>
2021-11-04 00:51:07 +0700

    Login

--- React/my-app/src/componentJS/Login/Login.js
+++ React/my-app/src/componentJS/Login/Login.js
@@ -7,14 +7,25 @@ function Login() {
             <div class="container">
                 <div class="left"></div>
                 <div class="right">
-                    <div id="">Welcome back!</div>
+                    <div id="welcome">Welcome back!</div>
                     
-                    <div class form>
-                        
+                    <div >                    
                         <div class="form">
+                            <div class = "inp">
+                                <span>Username</span>
+                                <input class = "impBx" type="text"></input>
+                            </div>                       
+                            <div class = "inp">
+                                <span>Password</span>
+                                <input class = "impBx" type="text"></input>
+                            </div> 
+                            <div class="remember">
+                                <label id = "checkbox"> <input type="checkbox"></input> Remember me</label>
+                                <a href="#">Forgot password?</a>
+                            </div>
                             <div class="btn-box">
-                                <button type="button" class="toggle-btn">Log in</button>
-                                <button type="button" class="toggle-btn">Sign up</button>
+                                <button type="button" class="toggle-btn1">Log in</button>
+                                <button type="button" class="toggle-btn2">Sign up</button>
 
                             </div>
                         </div>
--- React/my-app/src/componentJS/Login/SignUp.js
+++ React/my-app/src/componentJS/Login/SignUp.js
@@ -1,20 +1,34 @@
 import './CSS/Signup.css'
 
-function Login() {
+function Signup() {
 
     return (
         <div class="page">
             <div class="container">
                 <div class="left"></div>
                 <div class="right">
-                    <div id="">Welcome back!</div>
-                    
-                    <div class form>
-                        
+                    <div id="welcome">Create account</div>
+
+                    <div>
                         <div class="form">
+                            <div class="inp">
+                                <span>Username</span>
+                                <input class="impBx" type="text"></input>
+                            </div>
+                            <div class="inp">
+                                <span>Password</span>
+                                <input class="impBx" type="text"></input>
+                            </div>
+                            <div class="inp">
+                                <span>Retype Password</span>
+                                        <input class="impBx" type="text"></input>
+                            </div>
+                            <div class="remember">
+                                <label id="checkbox"> <input type="checkbox"></input> Agree with policy</label>
+                                <a href="#">Privacy Policy</a>
+                            </div>
                             <div class="btn-box">
-                                <button type="button" class="toggle-btn">Log in</button>
-                                <button type="button" class="toggle-btn">Sign up</button>
+                                <button type="button" class="toggle-btn1">Sign up</button>
 
                             </div>
                         </div>
