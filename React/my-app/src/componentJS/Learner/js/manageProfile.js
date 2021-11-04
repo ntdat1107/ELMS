173b7938ce023627108a2d2b24a5f2b29fca4fab
ntdat1107 <dat.ngo12345@hcmut.edu.vn>
2021-11-04 22:25:38 +0700

    1

--- React/my-app/src/componentJS/Learner/js/manageProfile.js
+++ React/my-app/src/componentJS/Learner/js/manageProfile.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "../css/LnManageProfile.css"
+import avtCheems from "../../../img/cheems.png"
+import ProfileCpn from "../../Instructor/profileCpn";
 
 function LnManageProfile() {
     return (
         <div id="lnManageProfile">
-            <p>ManageProfile Page.</p>
+            <ProfileCpn srcImage={avtCheems} name="LAM THANH DUONG" />
         </div>
     )
 }
