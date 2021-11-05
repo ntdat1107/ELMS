b1cc8d2afb5715d489489636302c31be7b3ed3a4
duonglam306 <lamduong11201@gmail.com>
2021-11-05 07:46:44 +0700

    update

--- React/my-app/src/componentJS/Learner/js/tableCourses.js
+++ React/my-app/src/componentJS/Learner/js/tableCourses.js
@@ -8,40 +8,56 @@ function TableCourse() {
   const [courses] = useState(listCourse.slice(0, 96));
   const [pageNumber, setPageNumber] = useState(0);
 
-  const coursesPerPage = 9;
+  const coursesPerPage = 12;
   const pagesVisited = pageNumber * coursesPerPage;
 
-  const displayCourses = courses
+  const bigCourses = courses
     .slice(pagesVisited, pagesVisited + coursesPerPage)
-    .map((course) => {
-      return (
-            <div id="displayCourse">
-              <div id="rowCourses">
-                <ItemCourse 
-                    idCourse={course.idCourse}
-                    nameCourse={course.nameCourse}
-                    authorCourse={course.authorCourse}
-                    progressScore = {course.progressScore}
-                    scoreRate={course.scoreRate}
-                />
-                <ItemCourse 
-                    idCourse={course.idCourse}
-                    nameCourse={course.nameCourse}
-                    authorCourse={course.authorCourse}
-                    progressScore = {course.progressScore}
-                    scoreRate={course.scoreRate}
-                /> 
-                <ItemCourse 
-                    idCourse={course.idCourse}
-                    nameCourse={course.nameCourse}
-                    authorCourse={course.authorCourse}
-                    progressScore = {course.progressScore}
-                    scoreRate={course.scoreRate}
-                /> 
-              </div>
-            </div>
-      );
-    });
+  const smallCourses = [];
+  for(let i = 0; i + 3 <= bigCourses.length; i = i + 3)
+  {
+    var temp = bigCourses.slice(i,i + 3);
+    smallCourses.push(temp);
+  }
+  console.log(smallCourses);
+  const smallDisplay = () => {
+    for(let i = 0; i < smallCourses.length; i++){
+      smallCourses[i].map((course) => {
+        console.log(course.idCourse);
+        return (
+          <div id="itemCourses">
+            <ItemCourse 
+                idCourse={course.idCourse}
+                nameCourse={course.nameCourse}
+                authorCourse={course.authorCourse}
+                progressScore = {course.progressScore}
+                scoreRate={course.scoreRate}
+            />
+          </div>
+        );
+      });
+    } 
+  }
+  const bigDisplay1 = () => {
+    return (
+      <div id="rowCourses">
+        {smallDisplay}
+      </div>
+    );
+  };
+  const bigDisplay = smallCourses[1].map((course) => {
+    return (
+      <div id="rowCourses">
+        <ItemCourse 
+            idCourse={course.idCourse}
+            nameCourse={course.nameCourse}
+            authorCourse={course.authorCourse}
+            progressScore = {course.progressScore}
+            scoreRate={course.scoreRate}
+        />
+      </div>
+    );
+  });
 
   const pageCount = Math.ceil(courses.length / coursesPerPage);
 
@@ -52,7 +68,7 @@ function TableCourse() {
   return (
       <div id="tableCourses">
           <Scrollbars id="scrollbars">
-          {displayCourses}
+          {bigDisplay}
           </Scrollbars>
           <ReactPaginate
               previousLabel={"Prev"}
