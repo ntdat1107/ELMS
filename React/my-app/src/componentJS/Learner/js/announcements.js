49f0cbe0df254475296baf7de8c9b33d64cb3e4f
duonglam306 <lamduong11201@gmail.com>
2021-11-06 14:47:39 +0700

    up

--- React/my-app/src/componentJS/Learner/js/announcements.js
+++ React/my-app/src/componentJS/Learner/js/announcements.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "../css/LnAnnouncements.css"
 import ListAnnoun from "./listAnnouncement"
-
+import Header from '../../header';
+import SideBarLearner       from '../../sideBarLearner';
+import avt from "../../../img/cheems.png";
 function LnAnnouncements() {
     return (
-        <div id="lnAnnouncements">
-            <ListAnnoun />
+        <div id="lnAnnouncementsUI">
+            <SideBarLearner/>
+            <Header linkAvt="/learner/manageprofile" link="/learner/dashboard" srcImg={avt} name="Lâm Thành Dương" gmail="lamduong11201@gmail.com" type="Learner"
+            idName="information"/>
+            <div id="lnAnnouncements">
+                <ListAnnoun />
+            </div>
         </div>
     )
 }
--- React/my-app/src/componentJS/Learner/js/readTutorial.js
+++ React/my-app/src/componentJS/Learner/js/readTutorial.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "../css/LnReadTutorial.css"
 import ReadTutorialCpn from "./readTutorialCpn"
+import Header from '../../header';
+import SideBarLearner       from '../../sideBarLearner';
+import avt from "../../../img/cheems.png";
 function LnReadTutorial() {
     return (
-        <div id="lnReadTutorial">
-            <ReadTutorialCpn />
+        <div id="lnReadTutorialUI">
+            <SideBarLearner/>
+            <Header linkAvt="/learner/manageprofile" link="/learner/dashboard" srcImg={avt} name="Lâm Thành Dương" gmail="lamduong11201@gmail.com" type="Learner"
+            idName="information"/>
+            <div id="lnReadTutorial">
+                <ReadTutorialCpn />
+            </div>
         </div>
     )
 }
