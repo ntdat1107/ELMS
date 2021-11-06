04fb9d9520f07134090b40983a981adcaa95dfe7
duonglam306 <lamduong11201@gmail.com>
2021-11-06 14:28:36 +0700

    update

--- React/my-app/src/componentJS/Learner/js/itemTutorial.js
+++ React/my-app/src/componentJS/Learner/js/itemTutorial.js
@@ -1,23 +1,34 @@
 import React from 'react'
 
 function ItemTutorial({
-    numberTutorial,
     courseTutorial,
-    nameTutorial,
+    titleTutorial,
     idCourseTutorial,
+    descTutorial,
+    idTutorial,
     callback = () => {}
 }) {
     return (
         <div className="itemTutorial" >
-            <div id="numOfTutorial"><p>{numberTutorial}</p></div>
-            <div className="courseTutorial" id = {"course" + idCourseTutorial}>
-                <p>{courseTutorial}</p>
+            <div id = {"imgTutorial" + idCourseTutorial}></div>
+            <div id="contentTutorial">
+                <div id="rowTutorialInfo1">
+                    <div className="courseTutorial" id = {"course" + idTutorial}>
+                        <p>{courseTutorial}</p>
+                    </div>
+                    <div id="titleTutorial">
+                        <p>{titleTutorial}</p>
+                    </div>
+                </div>
+                <div id="rowTutorialInfo2">
+                    <div id="descTutorial">
+                        <p>{descTutorial}</p>
+                    </div>
+                </div>
+                <div id="rowTutorialInfo3">
+                    <input type="button" id="btnReadTutorial" value = "Read more"/>
+                </div>
             </div>
-            <div id="nameTutorial">
-                <p>{nameTutorial} </p>
-            </div>
-            <input type="button" id="btnViewTutorial" value = "View PDF"/>
-            <input type="button" id="btnDownloadTutorial" value = "Download PDF"/>
         </div>
     )
 }
