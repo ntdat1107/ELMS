e138847bf73e678a5e3c7f332b824b397417de34
duonglam306 <lamduong11201@gmail.com>
2021-11-11 18:20:15 +0700

    up

--- React/my-app/src/Learner/dashboard/dashboard.js
+++ React/my-app/src/Learner/dashboard/dashboard.js
@@ -4,14 +4,14 @@ import About from "./about";
 import CourseForYou from "./courseForYou";
 import Calendar             from "../../DbCalendar"
 import Chart from "./chart"
-import HeaderLn from '../header/headerLn';
+import Header from '../../';
 import SideBarLearner       from '../sidebar/sideBarLearner';
 function LnDashboard() {
     return (
         <div id="lnDashBoardUI">
             <SideBarLearner/>
-            <HeaderLn linkAvt="/learner/manageprofile" link="/learner/dashboard" srcImg="http://placehold.jp/450x450.png" name="Lâm Thành Dương" gmail="lamduong11201@gmail.com" type="Learner"
-            idName="information"/>
+            <Header linkAvt="/learner/manageprofile" link="/learner/dashboard" srcImg="http://placehold.jp/450x450.png" name="Lâm Thành Dương" gmail="lamduong11201@gmail.com" type="Learner"
+            idName="information" typeUserTemp={1}/>
             <div id="lnDashboard">
                 <div id="about_CFY">
                     <About />
