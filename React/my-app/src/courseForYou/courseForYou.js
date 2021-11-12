d1530db6afe9b7bba90d8a292ee30c92b0ab3e54
duonglam306 <lamduong11201@gmail.com>
2021-11-12 18:53:30 +0700

    up

--- React/my-app/src/courseForYou/courseForYou.js
+++ React/my-app/src/courseForYou/courseForYou.js
@@ -36,7 +36,7 @@ function CourseForYou() {
                         imgSrcCourse = {git} 
                         altCourse = "GitCourseImage" 
                         Name = "Complete Linux Training Course to Get Your Dream IT Job 2021" 
-                        Desc="The BEST Linux Administration course that prepares you for corporate jobs and for RHCSA, RHCE, LFCS, CLNP certifications" 
+                        Desc="The BEST Linux Administration course that prepares you for corporate jobs and for RHCSA, RHCE, LFCS, CLNP certifications for RHCSA, RHCE, LFCS, CLNP certifications" 
                         Author="Mr. Long" 
                         Type="Best Course" 
                         rateScore="4.5" 
