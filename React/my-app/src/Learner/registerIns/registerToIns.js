61f4ec2f121cd27bc4466a67c39291d5a4e0be23
duonglam306 <lamduong11201@gmail.com>
2021-11-12 11:20:56 +0700

    up

--- React/my-app/src/Learner/registerIns/registerToIns.js
+++ React/my-app/src/Learner/registerIns/registerToIns.js
@@ -1,73 +1,70 @@
 import React from 'react'
 import "./LnRegisterInsForm.css";
 import { Link } from "react-router-dom"
-
+import uploadCV from "./uploadCV.png"
 function BoxRegisterIns() {
     return (        
         <div className="boxRegIns">
             <div className="Formcolumn">
                 <div >
-                    <div className="Formques">What kind of teaching have you done before?</div>
+                    <div className="Formques">1. What kind of teaching have you done before?</div>
                         <div id = "FormSelection">
-                            <label className="Formcontainer">In person, informally
-                                <input type="checkbox">
-                                </input>
-                                <span className="Formcheckmark"></span>
-                            </label>
-                            <label className="Formcontainer">In person, professionally
-                                <input type="checkbox"></input>
-                                <span className="Formcheckmark"></span>
-                            </label>
-                            <label className="Formcontainer">Online
-                                <input type="checkbox"></input>
-                                <span className="Formcheckmark"></span>
-                            </label>
-                            <label className="Formcontainer">Other
-                                <input type="checkbox"></input>
-                                <span className="Formcheckmark"></span>
-                            </label>
+                            <div className="Formcontainer">
+                                <input type="radio" id="op1" name="Question1" value="1"/>
+                                <label for="op1">In person, informally</label><br/>
+                            </div>
+                            <div className="Formcontainer">
+                                <input type="radio" id="op2" name="Question1" value="2"/>
+                                <label for="op2">In person, professionally</label><br/>
+                            </div>
+                            <div className="Formcontainer">
+                                <input type="radio" id="op3" name="Question1" value="3"/>
+                                <label for="op3">Online</label><br/>
+                            </div>
+                            <div className="Formcontainer">
+                                <input type="radio" id="op4" name="Question1" value="4"/>
+                                <label for="op4">Other</label><br/>
+                            </div>
                         </div>
                     </div>
                 <div >
-                    <div className="Formques" >How much of a video “pro” are you?</div>
+                    <div className="Formques" >2. How much of a video “pro” are you?</div>
                     <div id = "FormSelection">
-                        <label className="Formcontainer">I’m a beginner
-                            <input type="checkbox">
-                            </input>
-                            <span className="Formcheckmark"></span>
-                        </label>
-                        <label className="Formcontainer">I have some knowledge
-                            <input type="checkbox"></input>
-                            <span className="Formcheckmark"></span>
-                        </label>
-                        <label className="Formcontainer">I’m experienced
-                            <input type="checkbox"></input>
-                            <span className="Formcheckmark"></span>
-                        </label>
-                        <label className="Formcontainer">I have videos ready to up load
-                            <input type="checkbox"></input>
-                            <span className="Formcheckmark"></span>
-                        </label>
+                        <div className="Formcontainer">
+                            <input type="radio" id="op1" name="Question2" value="1"/>
+                            <label for="op1">I’m a beginner</label><br/>
+                        </div>
+                        <div className="Formcontainer">
+                            <input type="radio" id="op2" name="Question2" value="2"/>
+                            <label for="op2">I have some knowledge</label><br/>
+                        </div>
+                        <div className="Formcontainer">
+                            <input type="radio" id="op3" name="Question2" value="3"/>
+                            <label for="op3">I’m experienced</label><br/>
+                        </div>
+                        <div className="Formcontainer">
+                            <input type="radio" id="op4" name="Question2" value="4"/>
+                            <label for="op4">I have videos ready to up load</label><br/>
+                        </div>
                     </div>
                 </div>
             </div>
             <div className="Formcolumn">
                 <div >
-                    <div className="Formques">Do you have an audience to share your course with?</div>
+                    <div className="Formques">3. Do you have an audience to share your course with?</div>
                     <div id = "FormSelection">
-                        <label className="Formcontainer">Not at the moment
-                            <input type="checkbox">
-                            </input>
-                            <span className="Formcheckmark"></span>
-                        </label>
-                        <label className="Formcontainer">I have a small following
-                            <input type="checkbox"></input>
-                            <span className="Formcheckmark"></span>
-                        </label>
-                        <label className="Formcontainer">I have a sizeable following
-                            <input type="checkbox"></input>
-                            <span className="Formcheckmark"></span>
-                        </label>
+                        <div className="Formcontainer">
+                            <input type="radio" id="op1" name="Question3" value="1"/>
+                            <label for="op1">Not at the moment</label><br/>
+                        </div>
+                        <div className="Formcontainer">
+                            <input type="radio" id="op2" name="Question3" value="2"/>
+                            <label for="op2">I have a small following</label><br/>
+                        </div>
+                        <div className="Formcontainer">
+                            <input type="radio" id="op3" name="Question3" value="3"/>
+                            <label for="op3">I have a sizeable following</label><br/>
+                        </div>
                     </div>
                 </div>
                 <div >
@@ -75,7 +72,8 @@ function BoxRegisterIns() {
                     <div className="Formupload">
                         <p>Maximum file size: 100MB, maximum number of files: 3</p>
                         <div id="FormPushCV">
-                            <p>You can drag and drop files here to add them.</p>
+                            <img src={uploadCV} alt="Upload CV" style = {{paddingLeft: "45%"}} />
+                            <p style = {{paddingLeft: "10%"}}>You can drag and drop files here to add them.</p>
                         </div>
                     </div>
                 </div>
@@ -93,8 +91,8 @@ function BoxRegisterIns() {
                 </div>
             </div>
             <div className="FormSignupbtn-box">
-                <Link to="/learner/dashboard" ><p id="exitFormBtn">Exit</p></Link>
-                <Link to="/learner/dashboard" ><p id="submitFormBtn">Submit</p></Link>
+                <Link to="/learner/dashboard" className="Link-coursename" ><p id="exitFormBtn">Exit</p></Link>
+                <Link to="/learner/dashboard" className="Link-coursename" ><p id="submitFormBtn">Submit</p></Link>
             </div>
         </div>
     )
