a87f05886594005f354408a33bdfa5de060c4f21
duonglam306 <lamduong11201@gmail.com>
2021-11-12 21:13:12 +0700

    up

--- React/my-app/src/courseForYou/data.js
+++ React/my-app/src/courseForYou/data.js
@@ -8,25 +8,25 @@ import git1 from '../Learner/img/course/git1.jpg'
 import html1 from '../Learner/img/course/html1.jpg'
 import html2 from '../Learner/img/course/html2.jpg'
 import html3 from '../Learner/img/course/html3.jpg'
-import html4 from '../Learner/img/course/html4.jpg'
-import html5 from '../Learner/img/course/html5.jpg'
-import js1 from '../Learner/img/course/js1.jpg'
-import js2 from '../Learner/img/course/js2.jpg'
-import js3 from '../Learner/img/course/js3.jpg'
-import js4 from '../Learner/img/course/js4.jpg'
-import mobile1 from '../Learner/img/course/mobile1.jpg'
-import php1 from '../Learner/img/course/php1.jpg'
-import pygame1 from '../Learner/img/course/pygame1.jpg'
-import python1 from '../Learner/img/course/python1.jpg'
-import python2 from '../Learner/img/course/python2.jpg'
-import react1 from '../Learner/img/course/react1.jpg'
-import sql1 from '../Learner/img/course/sql1.jpg'
-import web1 from '../Learner/img/course/web1.jpg'
-import web2 from '../Learner/img/course/web2.jpg'
-import web3 from '../Learner/img/course/web3.jpg'
-import web4 from '../Learner/img/course/web4.jpg'
+// import html4 from '../Learner/img/course/html4.jpg'
+// import html5 from '../Learner/img/course/html5.jpg'
+// import js1 from '../Learner/img/course/js1.jpg'
+// import js2 from '../Learner/img/course/js2.jpg'
+// import js3 from '../Learner/img/course/js3.jpg'
+// import js4 from '../Learner/img/course/js4.jpg'
+// import mobile1 from '../Learner/img/course/mobile1.jpg'
+// import php1 from '../Learner/img/course/php1.jpg'
+// import pygame1 from '../Learner/img/course/pygame1.jpg'
+// import python1 from '../Learner/img/course/python1.jpg'
+// import python2 from '../Learner/img/course/python2.jpg'
+// import react1 from '../Learner/img/course/react1.jpg'
+// import sql1 from '../Learner/img/course/sql1.jpg'
+// import web1 from '../Learner/img/course/web1.jpg'
+// import web2 from '../Learner/img/course/web2.jpg'
+// import web3 from '../Learner/img/course/web3.jpg'
+// import web4 from '../Learner/img/course/web4.jpg'
 
-import star0 from '../Learner/img/rateStar/star.png'
+// import star0 from '../Learner/img/rateStar/star.png'
 import star0_5 from '../Learner/img/rateStar/star0_5.png'
 import star1 from '../Learner/img/rateStar/star1.png'
 import star1_5 from '../Learner/img/rateStar/star1_5.png'
@@ -60,8 +60,107 @@ const data =
     desc: "A-Z Guide to using CSS and CSS3 to enhance your web pages. Save time using CSS let us show you what CSS can do. Save time using CSS let us show you what CSS can do.",
     author: "Mr. Long", 
     type: "Best Course", 
+    rateScore: "0.5", 
+    imgStar: star0_5, 
+    totalRate: "(10,123)",
+    tagColor: styleH
+    },
+    {imgSrcCourse: css1, 
+    altCourse: "GitCourseImage", 
+    name: "Complete Linux Training Course to Get Your Dream IT Job 2021", 
+    desc: "A-Z Guide to using CSS and CSS3 to enhance your web pages. Save time using CSS let us show you what CSS can do. Save time using CSS let us show you what CSS can do.",
+    author: "Mr. Long", 
+    type: "Best Course", 
+    rateScore: "1", 
+    imgStar: star1, 
+    totalRate: "(10,123)",
+    tagColor: styleN
+    },
+    {imgSrcCourse: design1, 
+    altCourse: "GitCourseImage", 
+    name: "Complete Linux Training Course to Get Your Dream IT Job 2021", 
+    desc: "A-Z Guide to using CSS and CSS3 to enhance your web pages. Save time using CSS let us show you what CSS can do. Save time using CSS let us show you what CSS can do.",
+    author: "Mr. Long", 
+    type: "Best Course", 
+    rateScore: "1.5", 
+    imgStar: star1_5, 
+    totalRate: "(10,123)",
+    tagColor: styleBC
+    },
+    {imgSrcCourse: design2, 
+    altCourse: "GitCourseImage", 
+    name: "Complete Linux Training Course to Get Your Dream IT Job 2021", 
+    desc: "A-Z Guide to using CSS and CSS3 to enhance your web pages. Save time using CSS let us show you what CSS can do. Save time using CSS let us show you what CSS can do.",
+    author: "Mr. Long", 
+    type: "Best Course", 
+    rateScore: "2", 
+    imgStar: star2, 
+    totalRate: "(10,123)",
+    tagColor: styleBC
+    },
+    {imgSrcCourse: design3, 
+    altCourse: "GitCourseImage", 
+    name: "Complete Linux Training Course to Get Your Dream IT Job 2021", 
+    desc: "A-Z Guide to using CSS and CSS3 to enhance your web pages. Save time using CSS let us show you what CSS can do. Save time using CSS let us show you what CSS can do.",
+    author: "Mr. Long", 
+    type: "Best Course", 
+    rateScore: "2.5", 
+    imgStar: star2_5, 
+    totalRate: "(10,123)",
+    tagColor: styleBC
+    },
+    {imgSrcCourse: figma1, 
+    altCourse: "GitCourseImage", 
+    name: "Complete Linux Training Course to Get Your Dream IT Job 2021", 
+    desc: "A-Z Guide to using CSS and CSS3 to enhance your web pages. Save time using CSS let us show you what CSS can do. Save time using CSS let us show you what CSS can do.",
+    author: "Mr. Long", 
+    type: "Best Course", 
+    rateScore: "3", 
+    imgStar: star3, 
+    totalRate: "(10,123)",
+    tagColor: styleBC
+    },
+    {imgSrcCourse: git1, 
+    altCourse: "GitCourseImage", 
+    name: "Complete Linux Training Course to Get Your Dream IT Job 2021", 
+    desc: "A-Z Guide to using CSS and CSS3 to enhance your web pages. Save time using CSS let us show you what CSS can do. Save time using CSS let us show you what CSS can do.",
+    author: "Mr. Long", 
+    type: "Best Course", 
+    rateScore: "3.5", 
+    imgStar: star3_5, 
+    totalRate: "(10,123)",
+    tagColor: styleBC
+    },
+    {imgSrcCourse: html1, 
+    altCourse: "GitCourseImage", 
+    name: "Complete Linux Training Course to Get Your Dream IT Job 2021", 
+    desc: "A-Z Guide to using CSS and CSS3 to enhance your web pages. Save time using CSS let us show you what CSS can do. Save time using CSS let us show you what CSS can do.",
+    author: "Mr. Long", 
+    type: "Best Course", 
+    rateScore: "4", 
+    imgStar: star4, 
+    totalRate: "(10,123)",
+    tagColor: styleBC
+    },
+    {imgSrcCourse: html2, 
+    altCourse: "GitCourseImage", 
+    name: "Complete Linux Training Course to Get Your Dream IT Job 2021", 
+    desc: "A-Z Guide to using CSS and CSS3 to enhance your web pages. Save time using CSS let us show you what CSS can do. Save time using CSS let us show you what CSS can do.",
+    author: "Mr. Long", 
+    type: "Best Course", 
     rateScore: "4.5", 
     imgStar: star4_5, 
+    totalRate: "(10,123)",
+    tagColor: styleBC
+    },
+    {imgSrcCourse: html3, 
+    altCourse: "GitCourseImage", 
+    name: "Complete Linux Training Course to Get Your Dream IT Job 2021", 
+    desc: "A-Z Guide to using CSS and CSS3 to enhance your web pages. Save time using CSS let us show you what CSS can do. Save time using CSS let us show you what CSS can do.",
+    author: "Mr. Long", 
+    type: "Best Course", 
+    rateScore: "5", 
+    imgStar: star5, 
     totalRate: "(10,123)",
     tagColor: styleBC
     }
