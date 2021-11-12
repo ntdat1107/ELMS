fb37e7c8b37769515b3fde7964eca444545d88d2
ntdat1107 <dat.ngo12345@hcmut.edu.vn>
2021-11-12 15:37:21 +0700

    Update InsAnnouncement.js

--- React/my-app/src/Instructor/InsAnnouncement.js
+++ React/my-app/src/Instructor/InsAnnouncement.js
@@ -1,20 +1,21 @@
 import React from "react";
-import ListAnnoun from "../ListAnnounceCpn/ListAnnounceCpn";
-import "./CSS/InsAnnounment.css"
-import NewAnn from './new-ann'
 import Header from '../Header/header'
 import cheems from "../img/cheems.png"
 import SideBar from "../SideBar/SideBar";
+import ListAnnounceCpn from "../ListAnnounceCpn/ListAnnounceCpn";
+import AnnounData from "../Admin/FakedataforAnnoun"
+import NewAnnoun from "../Admin/NewAnnoun";
 
 
 function InsAnnouncement() {
     return (
-        <div id="adminannounce">
+        <div id="ins-announce-ui">
             <SideBar typeUserTemp={1}/>
             <Header linkAvt="/ins/manageprofile" link="/ins/dashboard" srcImg={cheems} name="Ngọ Tiến Đạt" gmail="tiendat_2001vn@gmail.com" type="Instructor"
         idName="information" typeUserTemp={1}/>
-            <NewAnn/>
-            <ListAnnoun/>
+            <div id="ins-announce">
+                <ListAnnounceCpn Addition_Part={<NewAnnoun />} FakeData={AnnounData} />
+            </div>
         </div>
     )
 }
