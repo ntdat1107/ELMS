fbce8876cc0b5d83c2e6ebd12b14d5a054233feb
ntdat1107 <dat.ngo12345@hcmut.edu.vn>
2021-11-15 09:14:03 +0700

    1

--- React/my-app/src/index.js
+++ React/my-app/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import ReportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'
+import store from './store';
+
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Provider store={store}>
     <App />
-  </BrowserRouter>,
+  </Provider>,
   document.getElementById('root')
 );
 
