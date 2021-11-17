4a717806f27f1b2d8bd58cd25c47f5b537e700e3
duonglam306 <lamduong11201@gmail.com>
2021-11-17 23:35:58 +0700

    up

--- React/my-app/src/Components/Header/searchBar.js
+++ React/my-app/src/Components/Header/searchBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 function SearchBar ({
     name,
     searchType,
@@ -8,8 +8,10 @@ function SearchBar ({
 }) {
     return (
         <div>
-          <input id="Input" type="text" name={name} placeholder={searchType}></input>
-          <img src={searchImg} alt="SearchIconImage" id="searchIcon" />
+            <input id="Input" type="text" name={name} placeholder={searchType}></input>
+            <Link className="Link-coursename" to="/learner/search/">
+                <img src={searchImg} alt="SearchIconImage" id="searchIcon" />
+            </Link>
         </div>
     )
 }
