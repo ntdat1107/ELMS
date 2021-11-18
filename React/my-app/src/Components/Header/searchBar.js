8c55db4c37a3d9e1e15a581e6917ca4b00f91767
hoangduy181 <hoang.duy.181001@gmail.com>
2021-11-18 20:09:52 +0700

    Update searchBar.js

--- React/my-app/src/Components/Header/searchBar.js
+++ React/my-app/src/Components/Header/searchBar.js
@@ -9,7 +9,7 @@ function SearchBar ({
     return (
         <div>
             <input id="Input" type="text" name={name} placeholder={searchType}></input>
-            <Link className="Link-coursename" to="/learner/search/">
+            <Link className="Link-coursename" to="/search/">
                 <img src={searchImg} alt="SearchIconImage" id="searchIcon" />
             </Link>
         </div>
