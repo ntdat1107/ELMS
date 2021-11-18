4aae752fa5b661c577c1550df5870dc48e164f7a
Jinnux <nguyentandat26032001@gmail.com>
2021-11-18 22:45:29 +0700

    404

--- React/my-app/src/Components/Page404/Page404.js
+++ React/my-app/src/Components/Page404/Page404.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import './Page404.css'
 
 function Page404() {
     return (
-        <div>
-            
-        </div>
+        <section className="Cases-404">
+            <h2 className="header404">404 Page not found</h2>
+            <p>The page you were looking for does not exist! Click <a href="index.html"> here</a> to return to Home Page. <br/>If you  got to this page by clicking a link, please notify us so we can fix this error!</p>
+        </section>
     )
 }
 
