0451bc38626ceecc211d537e668df8099e7a4ccd
ntdat1107 <dat.ngo12345@hcmut.edu.vn>
2021-11-20 11:07:11 +0700

    Update conversationActions.js

--- React/my-app/src/actions/conversationActions.js
+++ React/my-app/src/actions/conversationActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { CREATE_CVS_FAIL, CREATE_CVS_REQUEST, CREATE_CVS_SUCCESS } from "../constants/conversationConstants.js";
+import { CREATE_CVS_FAIL, CREATE_CVS_REQUEST, CREATE_CVS_SUCCESS, GET_RECEIVE_CVS_FAIL, GET_RECEIVE_CVS_REQUEST, GET_RECEIVE_CVS_SUCCESS, GET_SEND_CVS_FAIL, GET_SEND_CVS_REQUEST, GET_SEND_CVS_SUCCESS } from "../constants/conversationConstants.js";
 
 
 export const createNewCvs = (subject, content, fastName) => async(dispatch, getState) => {
@@ -24,6 +24,62 @@ export const createNewCvs = (subject, content, fastName) => async(dispatch, getS
     } catch (error) {
         dispatch({
             type: CREATE_CVS_FAIL,
+            payload: error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+        })
+    }
+}
+
+export const getSendCvs = () => async(dispatch, getState) => {
+    try {
+        dispatch({
+            type: GET_SEND_CVS_REQUEST
+        })
+        const { userLogin: { userInfo } } = getState() 
+
+        const config = {
+            headers: {
+                'Content-type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+        const { data } = await axios.get(`/api/conversations/getsend`, config)
+        dispatch({
+            type: GET_SEND_CVS_SUCCESS,
+            payload: data
+        })
+    } catch (error) {
+        dispatch({
+            type: GET_SEND_CVS_FAIL,
+            payload: error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+        })
+    }
+}
+
+export const getSendCvs = () => async(dispatch, getState) => {
+    try {
+        dispatch({
+            type: GET_RECEIVE_CVS_REQUEST
+        })
+        const { userLogin: { userInfo } } = getState() 
+
+        const config = {
+            headers: {
+                'Content-type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+        const { data } = await axios.get(`/api/conversations/getsend`, config)
+        dispatch({
+            type: GET_RECEIVE_CVS_SUCCESS,
+            payload: data
+        })
+    } catch (error) {
+        dispatch({
+            type: GET_RECEIVE_CVS_FAIL,
             payload: error.response && error.response.data.message
             ? error.response.data.message
             : error.message,
