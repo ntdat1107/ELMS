7b1002e5e25b589ff60cf41eb30230ded23c75ba
maiduclong11102001 <92844614+maiduclong11102001@users.noreply.github.com>
2021-11-23 13:38:20 +0700

    a

--- Backend/controllers.js/conversationController.js
+++ Backend/controllers.js/conversationController.js
@@ -3,66 +3,68 @@ import Conversation from "../models/conversationModel.js";
 import Course from "../models/courseModel.js";
 import User from "../models/userModel.js";
 
-const createConversation = asyncHandler(async(req, res) => {
-    let receivers = [], receiverName
+const createConversation = asyncHandler(async (req, res) => {
+    let receivers = [],
+        receiverName;
+    let senderName = req.user.firstName + " " + req.user.lastName;
     if (req.params.fastName == "toLearners") {
-        receivers = await User.find({isLearner: true})
-        receiverName = "All Learners"
+        receivers = await User.find({ isLearner: true });
+        receiverName = "All Learners";
     } else if (req.params.fastName == "toInstructors") {
-        receivers = await User.find({isIns: true})
-        receiverName = "All Instructors"
+        receivers = await User.find({ isIns: true });
+        receiverName = "All Instructors";
     } else {
-        const course = await Course.findOne({fastName: req.params.fastName})
-        for (let i=0; i<course.learnerList.length; i++) {
-            const temp = await User.findById(course.learnerList[i])
-            receivers.push(temp)
+        const course = await Course.findOne({ fastName: req.params.fastName });
+        for (let i = 0; i < course.learnerList.length; i++) {
+            const temp = await User.findById(course.learnerList[i]);
+            receivers.push(temp);
         }
-        receiverName = course.name
+        receiverName = course.name;
     }
     if (receivers && receivers.length > 0) {
-        const { subject, content } = req.body
-        const sender = await User.findById(req.user._id)
+        const { subject, content } = req.body;
+        const sender = await User.findById(req.user._id);
         const newConversation = await Conversation.create({
             subject,
             content,
             sender: req.user._id,
-            receiverName
-        })
+            senderName,
+            receiverName,
+        });
         if (newConversation) {
-            await newConversation.save()
-            sender.sendList.push(newConversation._id)
-            await sender.save()
-            receivers.map(receive => {
-                receive.receiveList.push(newConversation._id)
-                receive.save()
-            })
-            res.status(201).json(newConversation)
+            await newConversation.save();
+            sender.sendList.push(newConversation._id);
+            await sender.save();
+            receivers.map((receive) => {
+                receive.receiveList.push(newConversation._id);
+                receive.save();
+            });
+            res.status(201).json(newConversation);
         }
+    } else {
+        res.status(404);
+        throw new Error("Receiver not found or not exist");
     }
-    else {
-        res.status(404)
-        throw new Error('Receiver not found or not exist')
-    }
-})
+});
 
-const getSendList = asyncHandler(async(req, res) => {
-    const user = await User.findById(req.user._id)
-    let listCvs = []
-    for (let i=0; i<user.sendList.length; i++) {
-        const cvs = await Conversation.findById(user.sendList[i])
-        listCvs.push(cvs)
+const getSendList = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+    let listCvs = [];
+    for (let i = 0; i < user.sendList.length; i++) {
+        const cvs = await Conversation.findById(user.sendList[i]);
+        listCvs.push(cvs);
     }
-    res.status(200).json(listCvs)
-})
+    res.status(200).json(listCvs);
+});
 
-const getReceiveList = asyncHandler(async(req, res) => {
-    const user = await User.findById(req.user._id)
-    let listCvs = []
-    for (let i=0; i<user.receiveList.length; i++) {
-        const cvs = await Conversation.findById(user.receiveList[i])
-        listCvs.push(cvs)
+const getReceiveList = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+    let listCvs = [];
+    for (let i = 0; i < user.receiveList.length; i++) {
+        const cvs = await Conversation.findById(user.receiveList[i]);
+        listCvs.push(cvs);
     }
-    res.status(200).json(listCvs)
-})
+    res.status(200).json(listCvs);
+});
 
-export { createConversation, getSendList, getReceiveList }
+export { createConversation, getSendList, getReceiveList };
--- React/my-app/src/Components/ListAnnounceCpn/ContentCvs.js
+++ React/my-app/src/Components/ListAnnounceCpn/ContentCvs.js
@@ -1,41 +1,26 @@
-import { load } from "dotenv";
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getUserDetailByID } from "../../actions/userActions";
+import React from "react";
 
 function ContentCvs({ conversation, click, classN }) {
-    const dispatch = useDispatch();
-    const userDetailByID = useSelector((state) => state.userDetailByID);
-    const { loading, error, userInfoByID } = userDetailByID;
-    useEffect(() => {
-        dispatch(getUserDetailByID(conversation.sender));
-    }, [dispatch]);
-
     var time = conversation.createdAt.split("T");
 
-    if (loading) return <h1>Loading...</h1>;
-    else if (error) return <h1>ERROR</h1>;
-    else
-        return (
-            <>
-                <div
-                    id={conversation._id}
-                    className={click == conversation._id ? "choose active" : "choose"}
-                >
-                    <div id="sender-receiver">
-                        <p>{`From: ${userInfoByID && userInfoByID.firstName} ${
-                            userInfoByID && userInfoByID.lastName
-                        }`}</p>
-                        <p>To: {conversation.receiverName}</p>
-                    </div>
-                    <div id="inside-content-block">
-                        <p id="subject-inside">{conversation.subject}</p>
-                        <p id="content-inside">{conversation.content}</p>
-                        <p id="time-inside">{time[0]}</p>
-                    </div>
+    return (
+        <>
+            <div
+                id={conversation._id}
+                className={click == conversation._id ? "choose active" : "choose"}
+            >
+                <div id="sender-receiver">
+                    <p>From: {conversation.senderName}</p>
+                    <p>To: {conversation.receiverName}</p>
                 </div>
-            </>
-        );
+                <div id="inside-content-block">
+                    <p id="subject-inside">{conversation.subject}</p>
+                    <p id="content-inside">{conversation.content}</p>
+                    <p id="time-inside">{time[0]}</p>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default ContentCvs;
